a044435d2b5948c62353c593761698fdda21ed3b
RawdaFahim <rawdahfahim@gmail.com>
2022-08-10 16:30:11 +0200

    rawdA

--- dbms.sh
+++ dbms.sh
@@ -13,7 +13,7 @@ BYellow='\033[1;33m'
 BBlue='\033[1;34m'
 Purple='\033[1;35m'
 #addd something
-
+#New for merge
 #Calling the initializer function to give execute permission for all files
 chmod +x db_scripts/initializer.sh
 initialization
